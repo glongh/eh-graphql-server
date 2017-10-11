9028b09484e71862f4ad5fe4e1163c2e88312822
Gaston Longhitano <gaston@juntacorp.com>
2017-10-11 19:10:02 -0400

    Initial commit, setting up the repo with some files

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+const { graphql, buildSchema } = require('graphql');
+
+const PORT = process.env.PORT || 3000;
+const server = express();
+
+
+const schema = buildSchema(`
+type Video {
+    id: ID,
+    title: String,
+    duration: Int,
+    watched: Boolean
+}
+
+type Query {
+    video: Video
+    videos: [Video]
+}
+
+type Schema {
+    query: Query
+}
+`);
+
+const videoA = {
+    id:  'a',
+    title:  'Create a GraphQL Schema',
+    duration: 120,
+    watched:  true,
+}
+
+const videoB = {
+    id:  'b',
+    title:  'GraphQL Schema',
+    duration: 180,
+    watched:  false,
+}
+
+const videos = [ videoA, videoB ];
+
+const resolvers = {
+    video: () => ({
+        id:  '1',
+        title:  'bar',
+        duration: 180,
+        watched:  true,
+    }),
+    videos: () => videos,
+};
+
+server.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+    rootValue: resolvers
+}));
+
+server.listen(PORT, () => {
+    console.log('listening on http://localhost:3000');
+})
